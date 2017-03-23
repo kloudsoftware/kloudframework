fef5bfc00270e5804a694b4e9405f55dd5aec31d
Lars Grahmann <lars.grahmann@yahoo.de>
2017-03-23 19:05:07 +0100

    hashing

--- src/main/java/io/kloudwork/controller/LoginController.java
+++ src/main/java/io/kloudwork/controller/LoginController.java
@@ -5,6 +5,7 @@ import io.kloudwork.models.User;
 import io.kloudwork.util.MultipartFormHandler;
 import io.kloudwork.util.PostParamHolder;
 import org.apache.commons.fileupload.FileUploadException;
+import org.mindrot.jbcrypt.BCrypt;
 import spark.Request;
 import spark.Response;
 
@@ -14,11 +15,11 @@ import java.io.IOException;
 public class LoginController {
     private static LoginController ourInstance = new LoginController();
 
-    public static LoginController getInstance() {
-        return ourInstance;
+    private LoginController() {
     }
 
-    private LoginController() {
+    public static LoginController getInstance() {
+        return ourInstance;
     }
 
     public String login(Request request, Response response) {
@@ -33,8 +34,12 @@ public class LoginController {
         return "";
     }
 
-    public String postLogin(Request request, Response response) {
-        return "";
+    public String postLogin(Request request, Response response) throws IOException, FileUploadException {
+        PostParamHolder params = MultipartFormHandler.handle(request);
+        boolean matched = false;
+        matched = BCrypt.checkpw(params.getParameters().get("password"), "$2a$10$O7K72Aoa1sg8LCrzz2Iu1ejJTpCcHaK11tSmTgPbAimxmxQxbOkn.");
+
+        return String.valueOf(matched);
     }
 
     public String postLogout(Request request, Response response) {
@@ -44,9 +49,12 @@ public class LoginController {
     public String postRegister(Request request, Response response) throws IOException, FileUploadException {
         PostParamHolder params = MultipartFormHandler.handle(request);
 
+        String salt = BCrypt.gensalt();
+        String hash = BCrypt.hashpw(params.getParameters().get("password"), salt);
+
         User user = new User();
         user.setUsername(params.getParameters().get("username"));
-        user.setPassword(params.getParameters().get("password"));
+        user.setPassword(hash);
 
         EntityManager entityManager = Container.getInstance().getEntityManager();
         entityManager.getTransaction().begin();
