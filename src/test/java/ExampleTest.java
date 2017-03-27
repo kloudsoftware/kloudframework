63157f866b2055df77ee0ef1e6c4c58c8abcd2a3
Fred Lahde <fredlahde@gmail.com>
2017-03-27 16:48:21 +0200

    added testing utils

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,9 +1,23 @@
+import SpakTestUtil.SparkTestUtil;
+import io.kloudwork.MyWebApp;
+import spark.Spark;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ExampleTest {
 
     @org.junit.Test
-    public void example() {
+    public void example() throws Exception {
         assertTrue(true);
+
+        SparkTestUtil sparkTestUtil = new SparkTestUtil(4567);
+
+        new MyWebApp().start();
+        Spark.awaitInitialization();
+
+        SparkTestUtil.UrlResponse response =  sparkTestUtil.get("/");
+
+        assertEquals(200, response.status);
     }
 }
