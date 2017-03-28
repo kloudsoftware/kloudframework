bc43e825d8f6ed9b0cfd66b627309516e6ed99f4
Lars Grahmann <lars.grahmann@yahoo.de>
2017-03-28 11:01:57 +0200

    rolefilter works

---
+++ src/main/java/io/kloudwork/routes/filter/RoleFilter.java
@@ -0,0 +1,35 @@
+package io.kloudwork.routes.filter;
+
+import io.kloudwork.models.User;
+import io.kloudwork.persistence.UserRepository;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import java.util.List;
+
+public class RoleFilter implements Filter {
+
+    private List<String> allowedRoles;
+
+    public RoleFilter(List<String> rolesList) {
+        allowedRoles = rolesList;
+    }
+
+    @Override
+    public void handle(Request request, Response response) throws Exception {
+        String username = request.session().attribute("username");
+        if (username == null) {
+            throw Spark.halt(401, "User not authenticated");
+        }
+
+        UserRepository userRepository = new UserRepository();
+
+        User user = userRepository.findByUserName(username).orElseThrow(() -> Spark.halt(401, "User not found"));
+
+        if (!allowedRoles.contains(user.getRole())) {
+            throw Spark.halt(401, "User role not allowed");
+        }
+    }
+}
